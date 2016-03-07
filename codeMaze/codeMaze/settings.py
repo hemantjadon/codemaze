f3e284b1bdf7dfd13a6d30bd3f452f31a2b2c1d0
Hemant Jadon <hemantjadon.hemu@gmail.com>
2016-03-08 01:24:49 +0530

    deploy7

--- codeMaze/codeMaze/settings.py
+++ codeMaze/codeMaze/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'social.apps.django_app.default',
     'users',
     'questions',
+    'storages',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -137,7 +138,28 @@ USE_TZ = True
 
 STATIC_URL = '/dist/'
 
-STATIC_ROOT = '/var/www/codeMaze'
+AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'Cache-Control': 'max-age=94608000',
+}
+
+AWS_STORAGE_BUCKET_NAME = 'asn-files'
+AWS_ACCESS_KEY_ID = 'AKIAJKBJ3RJD452LQUNQ'
+AWS_SECRET_ACCESS_KEY = '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'
+
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'dist'),
